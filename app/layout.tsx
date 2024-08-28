f4310e6f3d2d22d67720d7061b6e4d8b794532e3
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-08-28 22:10:57 +0530

    added og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Modern Landing Page",
   description: "A modern Landing Page",
+  icons: {
+    icon: ["/assets/Logo.svg"],
+  },
+  openGraph: {
+    title: "Modern Landing Page",
+    description: "A modern Landing Page",
+    url: "https://nextjsmodernlp.vercel.app",
+    siteName: "Modern Landing Page",
+    images: [{ url: "/modern-og.png", width: 1200, height: 630 }],
+    locale: "en-IN",
+    type: "website",
+  },
 };
 
 const bricolage = Bricolage_Grotesque({
